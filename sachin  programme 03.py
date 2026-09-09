6135099a5aa0f54d9c7ea6590edfdb5f09140372
SachinSoriyal.18 <sachinsingh.11272@gmail.com>
2026-09-09 22:47:26 +0530

    Update sachin  programme 03.py

--- sachin  programme 03.py
+++ sachin  programme 03.py
@@ -2,19 +2,23 @@
 str1 =  " This is my tutorial.\t I create it into python"
 print(str1)
 
+
 # next line (using escape sequence button)
 str1 = " This is my tutorial.\n I create it into python"
 print(str1)
 
-# concatenation 
+
+# concatenation  which simple meaning is adding same data type variable
 # str1 = "sachin"
 # str2 = "singh"
 print(str1 + str2) 
+
 
 #length function for string 
 str1 = "sachin"
 len = len(str1)
 print(len)
+
 
 #role of tab when we use len function
 str1 = "sachin"
@@ -38,37 +42,46 @@ print(str[1: ])
 str =  "sachin"
 print(str[-3:-1])
 
+
 #string functions
 #01 (ends with function)
 str = "i am a coder"
 str1 =str.endswith("er")
 print(str1)
 
+
+#02
 str = "sachin singh"
 print(str.endswith("ingh"))
+
 
 # capitalize function
 str = "i am learning python"
 print(str.capitalize())
- 
+
+
 #in capatilize 
 str = "i am learning python"
 print(str.capitalize())
 print(str)
 
+
 #replace function 
 str  = "i am learning python "
 print (str.replace("a" , "o"))
 
+
 # replace function (word)
 str = "i am learning python in manual way"
 print(str.replace("python","javascript"))
+
 
 #find function
 str = "i am learning python from aapna college"
 print(str.find("o"))
 print(str.find("python")) 
 print(str.find("z"))
+
 
 #count function 
 str =  "i am learning python am  from am  aapna college"
@@ -84,18 +97,21 @@ if( a == b ):
     print(False)
 else:
     print(True)
-    
-01
+
+
+#01
 age = 20
 if(age >= 18):
     print("can vote and drive easily")
-    
+
+
 # 02
 age = 21
 if(age >= 18):
     print("apply for voting card and eligible for driving liscense")
     print("also can drive")
-    
+
+
 #03 (eg. of traffic Light )
 light = "green"
 if(light == "Red"):
@@ -136,6 +152,7 @@ elif( Room == "green"):
 else:
     print("darkness")
 
+
 #eg. 
 age = 24
 
@@ -158,7 +175,7 @@ else:
     print("grade'd'")
     
     
-02 way
+#02 way
 marks = int(input("enter student marks"))
 
 if(marks >= 90):
@@ -181,8 +198,3 @@ if(age >=18):
        print("cannot drive")
     else:
        print("can drive")
-       
-
-
-
-
