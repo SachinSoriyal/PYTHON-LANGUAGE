6287d672d70ba1cbfaf470aee06d105ef365f24b
SachinSingh.18 <sachinsingh.11272@gmail.com>
2026-09-10 23:45:54 +0530

    Add print statement for marks type
    Added print statement for marks type at the end.

--- sachin programme 02.py
+++ sachin programme 02.py
@@ -1,24 +1,29 @@
 # input in  python 
 name = (input("enter your name :")) 
 print("welcome", name)
- 
+
+
 # 02 type of input in type casting conversion 
 age = input("enter your age :")
 print("your age is ", age)
 
+
 # result of input is always string type
 val = input("enter a value :")
 print(type(val),val) 
+
 
 # type casting conversion for interger  value for input
 vaL = 7654
 val = int(input("enter your value:"))
 print(type(val),val)
 
+
 # type casting conversion for floating value
 val = 12.3467
 val = float(input("enter your value:"))
 print(type(val),val)
+
 
 # if we want to take the input of our age , name and marks
 name = input("enter your name :")
